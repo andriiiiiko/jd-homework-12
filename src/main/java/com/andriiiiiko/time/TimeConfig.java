514f5b1c06cb189b276a1632bc2afdf16af217b0
Andrii Protas <andreyprotas5@gmail.com>
2023-10-10 20:02:30 +0200

    Added .gitignore and TimeConfig.java

---
+++ src/main/java/com/andriiiiiko/time/TimeConfig.java
@@ -0,0 +1,25 @@
+package com.andriiiiiko.time;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class contains configuration constants related to time and timezones.
+ */
+public class TimeConfig {
+
+    public static final String DEFAULT_TIMEZONE = "UTC";
+    public static final String PARAM_TIMEZONE = "timezone";
+    public static final String CONTENT_TYPE_HTML = "text/html";
+    public static final int MILLISECONDS_IN_HOUR = 3600000; // 60 * 60 * 1000 :)
+    public static final List<String> VALID_TIMEZONES = Arrays.asList(
+            "UTC 0", "UTC 1", "UTC 2", "UTC 3", "UTC 4", "UTC 5", "UTC 6", "UTC 7", "UTC 8", "UTC 9", "UTC 10",
+            "UTC 11", "UTC 12", "UTC 13", "UTC-1", "UTC-2", "UTC-3", "UTC-4", "UTC-5", "UTC-6", "UTC-7", "UTC-8",
+            "UTC-9", "UTC-10", "UTC-11", "UTC-12"
+    );
+
+
+    private TimeConfig() {
+
+    }
+}
